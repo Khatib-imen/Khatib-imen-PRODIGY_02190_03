182bb15a8fe0eb8b41f0f012d4f668c7e2ca35d8
Khatib-imen <khatibimenkhatib10@gmail.com>
2024-07-19 16:13:34 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+// src/App.js
+import React from 'react';
+import Game from './components/Game';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="game-container">
+        <Game />
+      </div>
+    </div>
+  );
+}
+
+export default App;
